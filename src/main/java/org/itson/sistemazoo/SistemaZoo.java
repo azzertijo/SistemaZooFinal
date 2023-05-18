003b6fa4e997ad2577af1a3800e43731dcfb9aa8
azzertijo <azzertijo@outlook.com>
2023-05-18 14:52:01 -0700

    si
    si :+1:

--- src/main/java/org/itson/sistemazoo/SistemaZoo.java
+++ src/main/java/org/itson/sistemazoo/SistemaZoo.java
@@ -8,11 +8,15 @@ package org.itson.sistemazoo;
 import org.itson.implementacion.DesplegarSistema;
 
 /**
- *
+ * Clase principal que despliega el sistema entero
  * @author JORGE
  */
 public class SistemaZoo {
 
+    /**
+     * Método main que despliega el sistema
+     * @param args argumentos
+     */
     public static void main(String[] args) {
        new DesplegarSistema().desplegarSistema();
     }
